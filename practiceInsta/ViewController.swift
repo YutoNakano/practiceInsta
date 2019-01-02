bd5220006a79a2a8bf84432f0e24ada66b507ab7
Yuto Nakano <yuto.44.bb@gmail.com>
2019-01-02 21:10:36 +0900

    Initial Commit

---
+++ practiceInsta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practiceInsta
+//
+//  Created by 中野湧仁 on 2019/01/02.
+//  Copyright © 2019年 中野湧仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
